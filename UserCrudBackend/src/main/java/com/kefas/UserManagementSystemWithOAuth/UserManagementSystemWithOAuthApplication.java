0a6d45f3c9eabf3f0db256bfb9cfa9020ae43e54
Kefas Gisung <gisungkefas@gmail.com>
2022-12-31 20:55:59 +0100

    first commit

---
+++ UserCrudBackend/src/main/java/com/kefas/UserManagementSystemWithOAuth/UserManagementSystemWithOAuthApplication.java
@@ -0,0 +1,13 @@
+package com.kefas.UserManagementSystemWithOAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementSystemWithOAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementSystemWithOAuthApplication.class, args);
+	}
+
+}
---
+++ UserCrudBackend/src/test/java/com/kefas/UserManagementSystemWithOAuth/UserManagementSystemWithOAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kefas.UserManagementSystemWithOAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementSystemWithOAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
